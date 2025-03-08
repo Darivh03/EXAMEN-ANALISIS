8e9b0819a2770e496c7a1c0908803f8d45e67f82
Darivh03 <driverah2@miumg.edu.gt>
2025-03-08 15:54:52 -0600

    more change

--- web/views/template.php
+++ web/views/template.php
@@ -33,7 +33,7 @@ $path = TemplateController::path();
       include 'pages/contact/contact.php';
       include 'modules/footer.php';
       include 'modules/copyright.php';
-      include 'login.php';
+      include 'pages/login/login.php';
 
       ?>
     </div>
